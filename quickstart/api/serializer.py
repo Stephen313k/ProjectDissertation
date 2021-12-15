9f371b9466d7807d7f3b854e6bedb56c71e4e351
Stephen313k <38770008+Stephen313k@users.noreply.github.com>
2021-12-15 04:25:54 +0000

    Delete quickstart/api directory

--- quickstart/api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from quickstart.models import Student
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ['student_id', 'student_name', 'student_dob', 'student_year', 'student_school']
